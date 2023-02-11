e8c28f827e5a62cb19e0c54aabbb827670380149
matusstafura <kobe21@gmail.com>
2023-02-11 12:07:07 +0100

    improve resp menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,16 @@
+/* Burger menu */
 const burger = document.getElementById('burger');
 const resp = document.getElementById('resp');
+const main = document.getElementById('main');
+
 burger.addEventListener('click', () => {
     resp.classList.toggle('hidden');
+    burger.classList.toggle('hidden');
+});
+
+/* Close button */
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', () => {
+    resp.classList.toggle('hidden');
+    burger.classList.toggle('hidden');
 });
