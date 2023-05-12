a08a02f410830d5bc09c41d6963a5d515377fee4
matusstafura <kobe21@gmail.com>
2023-05-12 10:10:31 +0200

    introducing theme schemas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: [
         "./layouts/**/*.html",
+        "./content/**/*.html",
+        "./themes/berg/layouts/**/*.html",
+        "./themes/berg/content/**/*.{html,md}",
     ],
     darkMode: 'class', 
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+               primary: {
+                50: "var(--color-primary-50)",
+                100: "var(--color-primary-100)",
+                200: "var(--color-primary-200)",
+                300: "var(--color-primary-300)",
+                400: "var(--color-primary-400)",
+                500: "var(--color-primary-500)",
+                600: "var(--color-primary-600)",
+                700: "var(--color-primary-700)",
+                800: "var(--color-primary-800)",
+                900: "var(--color-primary-900)",
+                950: "var(--color-primary-950)",
+                },
+            },
+      },
     },
     plugins: [
         require('@tailwindcss/typography'),
